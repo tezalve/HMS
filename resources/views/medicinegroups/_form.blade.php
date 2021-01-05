8f6d0b54ac753f071bf0ca3f4d0b2adfcb8733f3
tezalve <tezalve@gmail.com>
2021-01-05 15:08:13 +0600

    small changes

--- resources/views/medicinegroups/_form.blade.php
+++ resources/views/medicinegroups/_form.blade.php
@@ -8,6 +8,6 @@
 
     <div class="col-lg-6 col-md-6 col-sm-6">
         <div class="col-lg-12 entry_panel_body">
-            <input type="submit" id="submit" name="submit" value="SAVE" class="col-lg-3 col-md-3 col-xs-3 btn btn-save btn-sm button button-save pull-right">
+            <input type="submit" id="submit" name="submit" value="SAVE" class="col-lg-3 col-md-3 col-xs-3 btn btn-save btn-sm button button-save">
         </div>
     </div>
